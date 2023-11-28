8eeb0ac5d57e126516f2fad8a307f95aaa1fa0e4
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2023-11-28 15:25:24 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.js
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -1,7 +1,7 @@
 let fs = require('fs');
-let inputData = fs.readFileSync('/dev/stdin').toString();
+let inputData = fs.readFileSync('/dev/stdin').toString().split("\n");
 
-let year = Number(inputData);
+let year = Number(inputData[0]);
 
 if((year%4 === 0 && year%100 !== 0) || year%400 === 0){
 	console.log(1); 
