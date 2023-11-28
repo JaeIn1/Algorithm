565eeffe0be0f4ba07b2b5df7352fce327a9e10e
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2023-11-28 14:44:02 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,8 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+console.log(a + b + c);
