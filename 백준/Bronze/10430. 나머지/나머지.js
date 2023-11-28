88eb1f95032ef1d37e126fc25037fb3a1bcd9e7e
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2023-11-28 14:28:08 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("/n");
+
+let line = input[0].split(" ");
+
+let a = Number(line[0]);
+let b = Number(line[1]);
+let c = Number(line[2]);
+
+console.log((a + b) % c);
+console.log(((a % c) + (b % c)) % c);
+console.log((a * b) % c);
+console.log(((a % c) * (b % c)) % c);
