6631b11e3862b24e464a1e35cb03fde06dc34183
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2023-12-05 19:59:34 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 9680 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.js
@@ -0,0 +1,11 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split("\n");
+let n = Number(input[0]);
+let scores = input[1].split(' ').map(Number);
+let maxValue = scores.reduce((a, b) => Math.max(a, b));
+let updated = [];
+for (let i = 0; i < n; i++) { // 수정된 원소 하나씩 저장
+updated.push(scores[i] / maxValue * 100);
+}
+// 배열에 포함된 원소의 평균 출력
+console.log(updated.reduce((a, b) => a + b) / n);
