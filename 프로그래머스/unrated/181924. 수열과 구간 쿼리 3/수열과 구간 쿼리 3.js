ffef5d5135be37220ec499d2cb131ce6a751a6ef
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-01-04 16:00:31 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.38 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,8 +1,6 @@
 function solution(arr, queries) {
-    for(let i  =0; i<queries.length; i++){
-        let temp = arr[queries[i][0]];
-        arr[queries[i][0]] = arr[queries[i][1]];
-        arr[queries[i][1]] = temp;
-    }
+    queries.forEach(([a,b])=>{
+        [arr[a],arr[b]] = [arr[b],arr[a]];
+    })
     return arr;
 }
