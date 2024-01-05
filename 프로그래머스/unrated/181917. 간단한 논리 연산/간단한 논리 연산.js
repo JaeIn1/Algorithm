1dba8bcf13ee2fa1de5439051cc36d3dec454164
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-01-05 15:18:47 +0900

    [level unrated] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -1,7 +1,3 @@
 function solution(x1, x2, x3, x4) {
-    let answer = true;
-    let a = x1 || x2;
-    let b = x3 || x4;
-    answer = a && b;
-    return answer;
+    return (x1 || x2) && (x3 || x4);
 }
