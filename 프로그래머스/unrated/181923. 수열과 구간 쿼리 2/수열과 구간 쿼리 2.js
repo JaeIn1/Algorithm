b65b49fdfd2fcff56ce9b9869d6629eeed64f7d3
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-01-05 13:12:20 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 9.42 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,12 +1,4 @@
 function solution(arr, queries) {
-    let result = [];
-    for(let i = 0; i<queries.length; i++){
-        let newArr = arr.slice(queries[i][0], queries[i][1]+1);
-        let x = newArr.filter((e)=>e > queries[i][2]);
-        if(x.length !== 0){
-            result.push(x.sort((a,b)=>a-b)[0]);
-        }else result.push(-1);
-        
-    }
+    let result = queries.map(([a,b,c]) => arr.slice(a , b + 1).filter((e) => e > c).sort((a , b) => a - b)[0] ?? -1);
     return result;
 }
