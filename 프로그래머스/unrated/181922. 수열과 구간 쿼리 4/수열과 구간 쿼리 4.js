7752db2117b7d0b431579a64af649d7663c8fca1
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-01-05 13:57:53 +0900

    [level unrated] Title: 수열과 구간 쿼리 4, Time: 4.68 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,6 +1,5 @@
 function solution(arr, queries) {
-    for(let i = 0; i < queries.length; i++){
-        let [a,b,c] = queries[i];
+    for(let [a, b, c] of queries){
         for(let i = a; i <= b; i++){
             if(i % c === 0) arr[i] += 1;
         }
