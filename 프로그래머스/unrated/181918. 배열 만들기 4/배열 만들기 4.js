3676de3abc59b0d1ac24f4424f55d1efa3db0a33
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-01-05 15:12:25 +0900

    [level unrated] Title: 배열 만들기 4, Time: 2.87 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -5,16 +5,15 @@ function solution(arr) {
     
     while(idx < arr.length){
         if(stk.length === 0){
-          stk.push(arr[idx]);
-          idx++;
-        }else{
+          stk.push(arr[idx++]);
+        }
+        else{
             if(stk[stk.length-1] < arr[idx]){
-              stk.push(arr[idx]);
-              idx++;
-          }
+              stk.push(arr[idx++]);
+            }
             else{
               stk.pop();
-          }
+            }
         }
     }
     return stk;
