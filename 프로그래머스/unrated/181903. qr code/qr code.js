a968ed81e5eefbada985d435b29d9f7a5f37b370
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-01-06 17:12:03 +0900

    [level unrated] Title: qr code, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.js
+++ 프로그래머스/unrated/181903. qr code/qr code.js
@@ -1,7 +1,3 @@
 function solution(q, r, code) {
-    let result = "";
-    for(let i = 0; i<code.length; i++){
-        if(i % q === r) result += code[i];
-    }
-    return result;
+    return code.split("").filter((_ , i) => i % q === r).join("");
 }
