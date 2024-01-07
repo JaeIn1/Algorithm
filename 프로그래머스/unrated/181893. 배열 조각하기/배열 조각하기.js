eca24e10d427bdc6da8e8764b70124f32e3ee8c3
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-01-07 15:16:59 +0900

    [level unrated] Title: 배열 조각하기, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
@@ -1,13 +1,6 @@
 function solution(arr, query) {
-    for(let i = 0 ; i < query.length ; i ++) {
-        const cur = query[i]
-        
-        if(i%2 === 0) {
-            arr.splice(cur+1, arr.length -(cur+1))
-        } else {
-             arr.splice(0, cur)
-        }
+    for(let i=0;i<query.length;i++){
+        i%2 ? arr.splice(0, query[i]) : arr.splice(query[i]+1);
     }
-    
     return arr
 }
