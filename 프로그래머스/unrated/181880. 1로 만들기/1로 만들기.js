19446a9e5fea36ea09f3223f92ff60b5abfad69c
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-01-08 17:26:42 +0900

    [level unrated] Title: 1로 만들기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -1,8 +1,7 @@
 function solution(num_list) {
     let result = 0;
     for(let i of num_list){
-        while(1){
-            if(i === 1) break;
+        while(i !== 1){
             if(i % 2 === 0) i /= 2;                      
             else i = (i -1) / 2;  
             result++;
