3f3e7903104cd15abeafd850e2134724526efd14
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-01-23 14:30:07 +0900

    [Silver III] Title: 주유소, Time: 296 ms, Memory: 36600 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -3,10 +3,11 @@ let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let num = Number(input[0]);
 let dis = input[1].split(" ").map(Number); 
+let disSum = dis.reduce((a,b) => a + b);
 let price = input[2].split(" ").map(Number);
 
 
-let result = 0;
+let result = BigInt(0);
 
 for(let i = 0; i < price.length -1; i++){
   if(price[i] < price[i+1]){
@@ -15,7 +16,7 @@ for(let i = 0; i < price.length -1; i++){
 }
 
 for(let i = 0; i < price.length -1; i++){
-  result += dis[i] * price[i];
+  result += BigInt(dis[i]) * BigInt(price[i]);
 }
 
-console.log(result);
+console.log(String(result));
