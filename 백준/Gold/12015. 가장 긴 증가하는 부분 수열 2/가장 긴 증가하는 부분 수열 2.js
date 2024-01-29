31c3ddc24b3d51062e46348795db21c367fce029
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-01-29 19:33:31 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 844 ms, Memory: 141624 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.js
@@ -0,0 +1,28 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]); // 데이터의 수(N)
+let arr = input[1].split(' ').map(Number);
+
+let d = [0];
+
+function lowerBound(arr, target , start, end){
+  while(start < end){
+    let mid = parseInt((start + end) / 2);
+    if(arr[mid] >= target) end = mid;
+    else{
+      start = mid + 1;
+    }
+  }
+  return end;
+}
+for(let i of arr){
+  if(d[d.length -1] < i){
+    d.push(i);
+  }
+  else{
+    let index = lowerBound(d, i , 0 , d.length);
+    d[index] = i;
+  }
+}
+console.log(d.length -1);
