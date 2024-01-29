341ce914b07370d586ffc7efe3cbf8705763e533
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-01-29 21:27:09 +0900

    [level 1] Title: 추억 점수, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,17 +1,9 @@
 function solution(name, yearning, photo) {
-    var answer = [];
+    let score = new Map();
     
-    let obj = {};
-    for(let i = 0; i< name.length; i++){
-        obj[name[i]] = yearning[i];
-    }
-    for(let i = 0; i< photo.length; i++){
-        let sum = 0;
-        for(let j of photo[i]){
-            if(obj[j]) sum += obj[j];
-            else continue;
-        }
-        answer.push(sum);
-    }
-    return answer;
+    name.forEach((e , i) => {
+        score.set(e,  yearning[i]);
+    });
+    
+    return photo.map((e) => e.reduce((a , n) => a + (score.get(n) ?? 0) , 0));
 }
