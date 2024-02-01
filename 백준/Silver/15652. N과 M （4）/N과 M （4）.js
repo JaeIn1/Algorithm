18703a0a8e1f536522a97da61a68c1a305d26322
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-02-01 13:49:40 +0900

    [Silver III] Title: N과 M (4), Time: 208 ms, Memory: 16604 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -0,0 +1,25 @@
+let fs = require("fs");
+let [n, m] = fs.readFileSync("/dev/stdin").toString().split(" ").map(Number);
+let arr = [];
+for (let i = 1; i <= n; i++) arr.push(i);
+let answer = "";
+let selected = [];
+
+function dfs(arr, depth ,start) {
+  let result = [];
+  
+  if (depth === m) {
+    for (let i of selected) result.push(arr[i]);
+    for (let x of result) answer += x + " ";
+    answer += "\n";
+    return;
+  }
+  for (let i = start; i < arr.length; i++) {
+    selected.push(i);
+    dfs(arr, depth + 1 , start);
+    selected.pop();
+    start++;
+  }
+}
+dfs(arr, 0, 0);
+console.log(answer);
