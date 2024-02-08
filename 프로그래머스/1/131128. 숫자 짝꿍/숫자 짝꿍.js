fb6e77c982f56d1b8ef257eb94c5a528bcc691bc
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-02-08 11:04:46 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,35 +1,24 @@
-function solution(X,Y){
-    let countX = new Array(10).fill(0);
-    const xArr = X.split("");
-    
-    xArr.forEach((item) => {
-        countX[item]++;
-    })
-    
-    let countY = new Array(10).fill(0);
-    const yArr = Y.split("");
-    
-    yArr.forEach((item) => {
-        countY[item]++;
-    })
-    
-    let ans = [];
-    
-    countX.forEach((item, index) => {
-        if(item !== 0 && countY[index] !== 0){
-            const min = Math.min(item, countY[index]);
-            
-            ans.push(String(index).repeat(min));
-        }
-    })
-    
-    if(ans.length === 0){
-        return "-1";
+function solution(x, y){
+  let answer = "";
+  const xHash = new Map();
+  const yHash = new Map();
+
+  x.split("")
+      .forEach(i=> xHash.set(i, xHash.has(i) ?  xHash.get(i)  +1 : 1));
+  y.split("")
+      .forEach(i=> yHash.set(i, yHash.has(i) ? yHash.get(i) +1 : 1));
+
+
+    for(let i = 9; i >= 0; i --){
+        const index = i.toString();
+        if(!xHash.has(index)) continue;
+        if(!yHash.has(index)) continue;
+
+        const num = Math.min(xHash.get(index), yHash.get(index))
+        answer += index.repeat(num)
+        //console.log(answer);
     }
-    
-    if(Number(ans.join("")) === 0){
-        return "0";
-    }
-    
-    return ans.reverse().join("");
+    if(answer.length === 0) return "-1"
+    if(answer[0] === "0" ) return "0";
+    return answer;
 }
