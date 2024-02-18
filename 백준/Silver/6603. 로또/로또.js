d3c2115ba5d4d640310f61cfbaa209bc69339d8a
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-02-18 12:41:58 +0900

    [Silver II] Title: 로또, Time: 184 ms, Memory: 12248 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.js
+++ 백준/Silver/6603. 로또/로또.js
@@ -1,35 +1,36 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
-let arr = [];
-for(let i = 0; i < input.length -1; i++){
-  arr.push(input[i].split(" ").map(Number));
-}
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 let visited = [];
-let select = [];
+let selected = [];
 let answer = "";
-for(let i of arr){
-  visited.push(i.map((e) => e === false))
-}
 
-function dfs(arr , depth , start , testcase){
-  let result = [];
-  if(depth === 6){
-    for(let i of select) result.push(arr[testcase][i]);
-    for(let j of result) answer += j + " ";
+for (let i = 0; i < input.length; i++) {
+  let data = input[i].split(" ").map(Number);
+  if (data[0] == 0) break;
+  else {
+    n = data[0];
+    arr = data.slice(1);
+    visited = new Array(n).fill(false);
+    selected = [];
+    answer = "";
+    dfs(arr, 0, 0);
+    console.log(answer);
+  }
+}
+function dfs(arr, depth, start) {
+  if (depth == 6) {
+    let result = [];
+    for (let i of selected) result.push(arr[i]);
+    for (let x of result) answer += x + " ";
     answer += "\n";
     return;
   }
-  for(let i = start; i < arr[testcase].length; i++){
-    if(visited[testcase][start]) continue;
-    visited[testcase][start] = true;
-    select.push(i);
-    dfs(arr , depth + 1, i + 1 , testcase);
-    visited[testcase][start] = false;
-    select.pop();
+  for (let i = start; i < arr.length; i++) {
+    if (visited[i]) continue;
+    selected.push(i);
+    visited[i] = true;
+    dfs(arr, depth + 1, i + 1);
+    selected.pop();
+    visited[i] = false;
   }
-}
-for(let i = 0; i < arr.length; i++){
-  answer = "";
-  dfs(arr , 0 , 1 , i);
-  console.log(answer);
 }
