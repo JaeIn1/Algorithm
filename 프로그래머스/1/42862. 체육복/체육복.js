61411dfa6a72fbe4cab945cbc87e26be7a60f08b
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-01 12:05:20 +0900

    [level 1] Title: 체육복, Time: 3.07 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,31 @@
+function solution(n, lost, reserve) {
+    var answer = 0;
+    let arr = new Array(n).fill(1);
+    
+    lost.forEach((e) =>{
+        arr[e - 1] -= 1;
+    })
+
+    reserve.forEach((e) =>{
+        arr[e - 1] += 1;
+    })
+    
+    lost.sort((a , b) => a - b);
+    
+    lost.forEach((e) =>{
+        if(arr[e - 1] === 1) return;
+        if(arr[e - 2] === 2){
+            arr[e - 2] -= 1;
+            arr[e - 1] += 1;
+        }
+        else if(arr[e] === 2){
+            arr[e] -= 1;
+            arr[e - 1] += 1;
+        }
+    })
+    arr.forEach((e) => {
+        if(e >= 1) answer++;
+    })
+    console.log(arr);
+    return answer;
+}
