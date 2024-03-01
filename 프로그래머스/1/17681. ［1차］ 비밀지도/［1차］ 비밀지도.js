ae0ae92bcc3738e3025f11822f8061630a8680a9
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-01 18:55:26 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,31 +1,3 @@
 function solution(n, arr1, arr2) {
-    let answer = [];
-    let result = new Array(arr1.length).fill([]);
-    arr1.forEach((e , i) =>{
-      let t = e.toString(2).padStart(n, "0");
-      let a = t.split("").map(Number)
-      let b = []
-      
-      for(let j = 0; j < a.length; j++){
-          if(a[j] === 0) b.push(" ");
-          else b.push("#");
-      }
-      result[i] = b;
-    });
-    
-    arr2.forEach((e , i) =>{
-      let t = e.toString(2).padStart(n, "0")
-      let a = t.split("").map(Number)
-      let b = []
-      
-      for(let j = 0; j < a.length; j++){
-          if(result[i][j] === "#") continue;
-          else if(result[i][j] === " " && a[j] === 1) result[i][j] = "#";
-      }
-    });
-    result.forEach((e , i) =>{
-        let r = e.join("");
-        answer.push(r);
-    })
-    return answer;
+    return arr1.map((e , i) => (e|arr2[i]).toString(2).padStart(n , 0).replace(/0/g , " ").replace(/1/g , "#"));
 }
