55e2aa421065437ffc3adf479364801ff0afa8f7
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-01 12:55:39 +0900

    [level 1] Title: 모의고사, Time: 26.10 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,16 +1,40 @@
+
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
 function solution(answers) {
     var answer = [];
-    var a1 = [1, 2, 3, 4, 5];
-    var a2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const man1 = [1, 2, 3, 4, 5];
+    const man2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const man3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let count = [0, 0, 0];
 
-    var a1c = answers.filter((a,i)=> a === a1[i%a1.length]).length;
-    var a2c = answers.filter((a,i)=> a === a2[i%a2.length]).length;
-    var a3c = answers.filter((a,i)=> a === a3[i%a3.length]).length;
-    var max = Math.max(a1c,a2c,a3c);
+    for(let i = 0; i < answers.length; i++) {
+        if(answers[i] == man1[i % man1.length]) count[0]++;
+        if(answers[i] == man2[i % man2.length]) count[1]++;
+        if(answers[i] == man3[i % man3.length]) count[2]++;
+    }
 
-    if (a1c === max) {answer.push(1)};
-    if (a2c === max) {answer.push(2)};
-    if (a3c === max) {answer.push(3)};
+    const max = Math.max(count[0], count[1], count[2]);
+    for(let i = 0; i < count.length; i++) {
+        if(max == count[i]) answer.push(i + 1);
+    }
+
     return answer;
 }
