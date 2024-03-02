512a5b193ab6165398670aa226af7a28a82c4169
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-02 23:38:52 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,7 @@
+function solution(phone_number) {
+    let answer = phone_number.split("");
+    for(let i = 0; i < phone_number.length - 4; i++){
+        answer[i] = "*";
+    }
+    return answer.join("");
+}
