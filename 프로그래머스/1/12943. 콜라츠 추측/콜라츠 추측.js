662b0549354e7161c3dcc429c64e34358552fb8b
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-03 00:31:20 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,16 +1,9 @@
 function solution(num) {
-    var answer = 0;
-    let i = 0;
-    while(num !== 1){
-        console.log(num);
-        if(i === 500){
-            answer = -1;
-            break;
-        }
-        if(num % 2 === 0) num /= 2;
-        else if(num % 2 === 1) num = num * 3 + 1;
-        answer++;
-        i++
+    let count = 0;
+    while(count < 500){
+        if(num === 1) return count;
+        count++;
+        num = num % 2 === 0 ? num /= 2 : num * 3 + 1;
     }
-    return answer;
+    return -1;
 }
