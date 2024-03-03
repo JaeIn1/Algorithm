adc4898cf7c62b159e5c65156eb5dd775f7cb250
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-03 23:39:54 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,11 +1,19 @@
 function solution(s) {
-    var answer = true;
-    let arr = s.split("");
-    arr.forEach((e) =>{
-        if(isNaN(e)) answer = false;
-    })
-    if(arr.length !== 4 && arr.length !== 6) answer = false;
-    
-    
+    var answer = true; // 기본적으로 true로 설정
+
+    // 문자열의 각 문자가 숫자인지 확인
+    for (var i = 0; i < s.length; i++) {
+        // 현재 문자가 숫자가 아닌 경우
+        if (isNaN(parseInt(s[i]))) {
+            answer = false; // 결과를 false로 설정
+            break; // 더 이상 확인할 필요 없으므로 반복문 탈출
+        }
+    }
+
+    // 문자열의 길이가 4 또는 6인지 확인
+    if (s.length !== 4 && s.length !== 6) {
+        answer = false; // 길이가 4 또는 6이 아니면 false로 설정
+    }
+
     return answer;
 }
