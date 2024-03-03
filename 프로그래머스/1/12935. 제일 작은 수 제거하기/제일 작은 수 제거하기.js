52aae9a3027abbb50e64fcaa1f403aff1b53a1fa
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-03 17:59:02 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,11 +1,7 @@
 function solution(arr) {
     if(arr.length === 1) return [-1];
     else{
-        let temp = [...arr];
-        temp.sort((a , b) => a - b);
-        let index = arr.indexOf(temp[0]);
-        arr.splice(index , 1);
-        
+        arr.splice(arr.indexOf(Math.min(...arr)) , 1);
     }
     return arr;
 }
