226cd1f542dc4bc44c1f6f6d31eae3e697925fb7
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-03 18:32:55 +0900

    [level 1] Title: 약수의 합, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,10 +1,10 @@
 function solution(n) {
-    let answer = [];
+    let answer = 0;
     for(let i = 1; i <= Math.sqrt(n); i++){
         if(n % i === 0){
-            answer.push(i);
-            if(n / i !== i) answer.push(n / i);
+            answer += i;
+            if(n / i !== i) answer += n / i;
         }
     }
-    return answer.reduce((a , b) => a + b , 0);
+    return answer;
 }
