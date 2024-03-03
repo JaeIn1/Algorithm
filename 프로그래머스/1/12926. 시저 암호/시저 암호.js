75d5909b1c3514a945010cece42fae9bb5d2fa62
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-03 18:59:06 +0900

    [level 1] Title: 시저 암호, Time: 4.82 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,26 +1,17 @@
 function solution(s, n) {
-    let answer = '';
-    let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let lower = "abcdefghijklmnopqrstuvwxyz";
-    
-    let arr = s.split(" ");
-    arr.forEach((e , i) =>{
-        let t = e.split("");
-        let temp = "";
-        t.forEach((e , i) =>{
-            if(upper.includes(e)){
-                let index = upper.indexOf(e) + n;
-                if(index >= upper.length) index -= upper.length;
-                temp += upper[index];
-            }
-            else if(lower.includes(e)){
-                let index = lower.indexOf(e) + n;
-                if(index >= lower.length) index -= lower.length;
-                temp += lower[index];
-            }
-        })
-        arr[i] = temp;
-    })
-    
-    return arr.join(" ");
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var answer = "";
+  for (var i = 0; i < s.length; i++) {
+    var text = s[i];
+    if (text == " ") {
+      answer += " ";
+      continue;
+    }
+    var textArr = upper.includes(text) ? upper : lower;
+    var index = textArr.indexOf(text) + n;
+    if (index >= textArr.length) index -= textArr.length;
+    answer += textArr[index];
+  }
+  return answer;
 }
