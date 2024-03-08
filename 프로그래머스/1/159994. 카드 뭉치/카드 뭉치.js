71bad4ba1dd352e287687649d30478994406d566
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-08 20:42:41 +0900

    [level 1] Title: 카드 뭉치, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,13 +1,12 @@
 function solution(cards1, cards2, goal) {
     var answer = '';
-    
-    for(let i = 0; i < goal.length; i++){
-        let str = goal[i];
-        
-        if(cards1[0] === str) cards1.shift();   
-        else if(cards2[0] === str) cards2.shift();    
-        else return "No";    
-    }
-    
-    return "Yes";
+    let flag = true;
+    goal.forEach((e , i) =>{
+        if(cards1[0] === e) cards1.shift();
+        else if(cards2[0] === e) cards2.shift();
+        else flag = false;
+    });
+    answer = "Yes";
+    if(!flag) return "No";
+    return answer;
 }
