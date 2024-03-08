db2375e125ff1af3974a18f9e2c8af06e96977c6
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-08 19:06:15 +0900

    [level 1] Title: 덧칠하기, Time: 6.34 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,18 +1,16 @@
 function solution(n, m, section) {
     let cnt = 0;
-    let start = section[0];
-    let arr = new Array(n).fill(true);
-    for(let i of section){
-        arr[i-1] = false;
-    }
+    let arr=  new Array(n).fill(true);
+    for(let i = 0; i < section.length; i++) arr[section[i] - 1] = false;
     
-    section.forEach((e,i) =>{
-        if(arr[e - 1] === false){
-            for(let i = 0; i < m; i++){
-                arr[e -1 + i] = true;   
+    section.forEach((e) =>{
+        if(!arr[e - 1]){
+            for(let i = e - 1; i < e - 1 + m; i++){
+                arr[i] = true;
             }
             cnt++;
         }
     })
+    
     return cnt;
 }
