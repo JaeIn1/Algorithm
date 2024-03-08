c607c56330de1b2fbed5a4c024356b656bf43cf9
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-08 15:57:11 +0900

    [level 1] Title: 공원 산책, Time: 0.43 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,17 +1,14 @@
 function solution(park, routes) {
-  const parkHigh = park.length;
-  const parkWidth = park[0].length;
-
-  // 시작지점 인덱스 찾기
- let start;
-  for (let i = 0; i < parkHigh; i++) {
-    if (park[i].indexOf("S") !== -1) {
-      start = [i, park[i].indexOf("S")];
-      break;
+    let width = park[0].length;
+    let height = park.length;
+    let start;
+    for(let i = 0 ; i < height; i++){
+        if(park[i].indexOf("S") !== -1){
+            start = [i, park[i].indexOf("S")];
+            break;
+        }
     }
-  }
     
-  // 각 명령어로 이동할 방향
     const directions = {
     E: [0, 1],
     W: [0, -1],
@@ -19,24 +16,22 @@ function solution(park, routes) {
     N: [-1, 0],
   };
     
-
-  for (const route of routes) {
-    const [dir, distanceStr] = route.split(" ");
-    let distance = parseInt(distanceStr);
-    let [x, y] = start;
-
-    let step = 0;
-    
-    // 명령어에 적힌 distance만큼 모두 이동할 때까지 반복
-    while (step < distance) {
-      x += directions[dir][0];
-      y += directions[dir][1]; 
-      if(x < 0 || parkHigh <= x || y < 0 || parkWidth <= y || park[x][y] === "X") break;
-      step++;
-    }
-      
-    if (step === distance) start = [x, y];
-  }
+    routes.forEach((e , i) =>{
+        let [r , n] = e.split(" ");
+        let distance = parseInt(n);
+        let [x , y] = start;
+        let step = 0;
+        
+        while(step < distance){
+            x += directions[r][0];
+            y += directions[r][1];
+            if(x < 0 || x >= height || y < 0 || y >= width || park[x][y] === "X") break;
+            step++
+        }
+        
+        if(step === distance) start = [x , y];
+        
+    })
 
   return start;
 }
