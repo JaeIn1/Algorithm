47236be4adc46979ea7c09514fe279385de9e5cd
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-09 17:45:34 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.66 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,16 +1,17 @@
 function solution(survey, choices) {
     var answer = '';
-    let indi = new Map();
-    ['R','T', 'C','F', 'J','M', 'A', 'N'].forEach(item => indi.set(item, 0));
-
-    choices.forEach((item,index) => {
-        let [A, B] = survey[index].split('');
-        if(item > 4) indi.set(B, indi.get(B)+item-4);
-        else if(item < 4) indi.set(A, indi.get(A)+4-item);
-    })
-    answer += indi.get('R') >= indi.get('T') ? 'R' : 'T';
-    answer += indi.get('C') >= indi.get('F') ? 'C' : 'F';
-    answer += indi.get('J') >= indi.get('M') ? 'J' : 'M';
-    answer += indi.get('A') >= indi.get('N') ? 'A' : 'N';
+    let map = new Map();
+    ['R','T', 'C','F', 'J','M', 'A', 'N'].forEach((item) => map.set(item , 0));
+    choices.forEach((e,i) => {
+        let [a,b] = survey[i].split('');
+        let num = choices[i];
+        if(e > 4) map.set(b , map.get(b) + num - 4);
+        else if(e < 4) map.set(a , map.get(a) + 4 - num);
+    });
+    answer += map.get('R') >= map.get('T') ? 'R' : 'T';
+    answer += map.get('C') >= map.get('F') ? 'C' : 'F';
+    answer += map.get('J') >= map.get('M') ? 'J' : 'M';
+    answer += map.get('A') >= map.get('N') ? 'A' : 'N';
+    
     return answer;
 }
