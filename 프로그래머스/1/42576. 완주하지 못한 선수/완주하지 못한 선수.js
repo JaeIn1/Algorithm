81c66602c8704210a6d1c5199ea8651587564fff
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-13 14:21:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 56.78 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,18 +1,16 @@
 function solution(participant, completion) {
     var answer = '';
-    
     let map = new Map();
     participant.forEach((e) =>{
-        map.set(e , map.has(e) ? map.get(e) + 1 : 1);
+        map.set(e , map.get(e) ? map.get(e) + 1 : 1);
     });
     
     completion.forEach((e) =>{
-        map.set(e , map.get(e) - 1);
-    })
+        map.set(e , map.get(e) - 1)
+    });
     
     for(let [key , value] of map){
-        if(value === 0) continue;
-        else answer = key;
+        if(value !== 0) answer = key;
     }
     return answer;
 }
