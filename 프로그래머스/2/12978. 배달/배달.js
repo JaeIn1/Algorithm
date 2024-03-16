197755579758c1fd42a3d16059e24315a7dfb1ae
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-16 18:35:53 +0900

    [level 2] Title: 배달, Time: 4.11 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,27 +1,26 @@
 function solution(N, road, K) {
     var answer = 0;
-    const arr = Array(N + 1).fill(Number.MAX_SAFE_INTEGER);
-    const lines = Array.from(Array(N + 1), () => []);
-
-  road.forEach((value) => {
-    // 연결되어 있는 경로를 모두 lines배열에 저장한다.
-    let [a, b, c] = value;
-    lines[a].push({ to: b, cost: c });
-    lines[b].push({ to: a, cost: c });
-  });
+    let arr = new Array(N + 1).fill(Number.MAX_SAFE_INTEGER);
+    let lines = Array.from({length : N + 1}, ()=>[]);
     
-    let queue = [{ to: 1, cost: 0 }];
+    road.forEach((e) =>{
+        let [a , b , c] = e;
+        lines[a].push({to : b , cost : c})
+        lines[b].push({to : a , cost : c})
+    });
+    
+    let queue = [{to: 1 , cost : 0}];
     arr[1] = 0;
     
-    while (queue.length) {
-    // queue가 빈 배열이 될 때까지 반복
-    let { to } = queue.pop();
-    for(let line of lines[to]){
-        if(arr[line.to] > arr[to] + line.cost){
-            arr[line.to] = arr[to] + line.cost;
-            queue.push(line);
+    while(queue.length){
+        let {to} = queue.pop();
+        for(let line of lines[to]){
+            if(arr[line.to] > arr[to] + line.cost){
+                arr[line.to] = arr[to] + line.cost;
+                queue.push(line);
+            }
         }
     }
-  }
+    console.log(arr);
     return arr.filter((e) => e <= K).length;
 }
