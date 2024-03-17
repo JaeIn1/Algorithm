524a7a108ac9b77e88b8cce95c0b63f5a3ab7f31
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-17 11:38:39 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,13 @@
+function solution(n)
+{
+    var ans = 0;
+    while(n > 0){
+        if(n % 2 === 0) n /= 2;
+        else{
+            ans++;
+            n -= 1;
+        }
+    }
+
+    return ans;
+}
