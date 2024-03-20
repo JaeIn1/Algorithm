4dd8cb5f7b960a3933831c928bfcf5ac2c9f4212
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-20 13:01:36 +0900

    [level 2] Title: 구명보트, Time: 10.12 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,16 +1,19 @@
 function solution(people, limit) {
-    const sortPeople = people.sort((a,b)=>a-b);
-    let answer = 0;
-    let left = 0, right = people.length-1;
+    // 몸무게순으로 정렬
+    people.sort((a, b) => a - b);
+    
+    let boats = 0;
+    let left = 0;
+    let right = people.length - 1;
     
     while (left <= right) {
-        if (sortPeople[left] + sortPeople[right] <= limit) {
-            left++;
-            right--;
-        } else {
-            right--;
+        // 가장 무거운 사람과 가장 가벼운 사람을 합쳐서 태울 수 있는지 확인
+        if (people[left] + people[right] <= limit) {
+            left++; // 가장 가벼운 사람을 태웠으니 다음으로 넘어감
         }
-        answer++;
+        right--; // 가장 무거운 사람은 항상 태워야 하므로 다음으로 넘어감
+        boats++; // 구명보트 개수 증가
     }
-    return answer;
+    
+    return boats;
 }
