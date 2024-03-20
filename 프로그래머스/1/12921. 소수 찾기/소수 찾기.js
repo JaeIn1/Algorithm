09af4e3bc18f323d79ae9dda680095c1ab44dfd4
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-20 16:34:03 +0900

    [level 1] Title: 소수 찾기, Time: 64.67 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -2,14 +2,11 @@ function solution(n) {
     var answer = 0;
     let arr = [];
     
+    for(let i = 2; i <= n; i++) arr[i] = i;
+    
     for(let i = 2; i <= n; i++){
-        arr[i] = i;    
+        if(arr[i] === 0) continue;
+        for(let j = i + i; j <= n; j+=i) arr[j] = 0;  
     }
-    for(let j = 2; j <= n; j++){
-        if(arr[j] === 0) continue;
-        for(let k = j + j; k <= n; k+=j){
-            arr[k] = 0;
-        }
-    }
-    return arr.filter((e) => e).length;
+    return arr.filter((e) => e).length;;
 }
