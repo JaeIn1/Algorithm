bedf6b8852c4ebe31437c75416b0a27a26685cfd
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-03-20 19:13:58 +0900

    [level 1] Title: 하샤드 수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -2,7 +2,6 @@ function solution(x) {
     var answer = true;
     let arr = x.toString().split("").map(Number);
     let sum = arr.reduce((a , b) => a + b , 0);
-    if(x % sum === 0) answer = true;
-    else answer = false;
+    answer = x % sum === 0? true : false;
     return answer;
 }
