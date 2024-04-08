f0158c9b01188b3359c6037bf58e6d7eeea2b79c
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-04-08 18:17:43 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -4,9 +4,12 @@ let n = Number(input[0]);
 let answer = 0;
 
 function Fibo(num){
-  if(num === 0) return 0;
-  if(num === 1) return 1;
-  return Fibo(num - 1) + Fibo(num - 2);
+  if(num < 2) return num;
+  let dp = [0 , 1];
+  for(let i = 2; i <= num; i++){
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[num];
 }
 function solution(data){
   return Fibo(data);
