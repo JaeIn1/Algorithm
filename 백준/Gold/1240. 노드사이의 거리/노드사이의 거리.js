62c086e3a210098b1ff707d01d4f550ccc3fc6a8
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-04-13 20:32:26 +0900

    [Gold V] Title: 노드사이의 거리, Time: 376 ms, Memory: 14980 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.js
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.js
@@ -1,28 +1,30 @@
 let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-let [n, m] = input[0].split(" ").map(Number); // 노드의 개수(N), 쿼리의 개수(M)
-let graph = []; // 트리 정보 입력받기
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let [n , m] = input[0].split(" ").map(Number);
+
+let graph = [];
 let visited = new Array(n + 1).fill(false);
-let distance = new Array(n + 1).fill(-1);
+let distanse = new Array(n + 1).fill(0);
 
-for (let i = 1; i <= n; i++) graph[i] = [];
-for (let i = 1; i < n; i++) {
-  // 노드 X와 노드 Y는 서로 연결
-  let [x, y, cost] = input[i].split(" ").map(Number);
-  graph[x].push([y, cost]);
-  graph[y].push([x, cost]);
-}
-function dfs(x, dist) {
-  if (visited[x]) return;
-  visited[x] = true;
-  distance[x] = dist;
-  for (let [y, cost] of graph[x]) dfs(y, dist + cost);
+for(let i = 1; i <= n; i++) graph[i] = [];
+for(let i = 1; i < n; i++){
+  let [a , b , cost] = input[i].split(" ").map(Number);
+  graph[a].push([b , cost]);
+  graph[b].push([a , cost]);
 }
 
-for (let i = 0; i < m; i++) {
-  let [x, y] = input[i + n].split(" ").map(Number);
-  dfs(x, 0);
-  console.log(distance[y]);
+function dfs(node , dist){
+  if(visited[node]) return;
+  visited[node] = true;
+  distanse[node] += dist;
+  for(let [y,  cost] of graph[node])dfs(y , dist + cost);
+  
+}
+
+for(let i = n; i < n + m; i++){
+  let [start , end] = input[i].split(" ").map(Number);
+  dfs(start , 0);
+  console.log(distanse[end]);
   visited = new Array(n + 1).fill(false);
-  distance = new Array(n + 1).fill(-1);
+  distanse = new Array(n + 1).fill(0);
 }
