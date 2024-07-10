3364ce73370fbaa3a588e7bc81b49179c87c0a52
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-07-10 21:08:51 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,13 +1,4 @@
 function solution(clothes) {
-    let map = new Map();
-    let result = 1;
-    
-    clothes.forEach((e) =>{
-        map.set(e[1] , map.has(e[1]) ? map.get(e[1]) + 1  : 1);
-    });
-    
-    for(let [key , value] of map){
-        result *= value + 1;
-    }
-    return result - 1;
+    var answer = 0;
+    return answer;
 }
