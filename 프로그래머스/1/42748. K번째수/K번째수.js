d798191bee2ad69dda87bf4ba20e2976c9b0229d
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-07-19 16:36:33 +0900

    [level 1] Title: K번째수, Time: 0.12 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,9 @@
 function solution(array, commands) {
     var answer = [];
-    commands.forEach((e) =>{
-        let [a , b , c] = e;
-        let temp = array.slice(a - 1 , b);
-        temp.sort((a , b) => a - b);
-        answer.push(temp[c - 1]);
+    commands.forEach((e) => {
+        let [start , end , k] = e;
+        let arr = array.slice(start - 1 , end).sort((a , b) => a - b);
+        answer.push(arr[k - 1]);
     })
     return answer;
 }
