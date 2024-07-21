a4080252a1708a413b6e747cc5410850f4b0c038
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-07-21 17:20:46 +0900

    [level 1] Title: 모의고사, Time: 1.55 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,19 +1,20 @@
 function solution(answers) {
-    var answer = [[1 , 0] ,[2 , 0], [3 , 0]];
+    let count = [0 , 0 , 0];
     let p1 = [1, 2, 3, 4, 5];
     let p2 = [2, 1, 2, 3, 2, 4, 2, 5];
     let p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
     answers.forEach((e , i) =>{
-        if(e === p1[(i) % p1.length]) answer[0][1]++;
-        if(e === p2[(i) % p2.length]) answer[1][1]++;
-        if(e === p3[(i) % p3.length]) answer[2][1]++;
+        if(e === p1[(i) % p1.length]) count[0]++;
+        if(e === p2[(i) % p2.length]) count[1]++;
+        if(e === p3[(i) % p3.length]) count[2]++;
     });
     
-    let max = 0;
-    answer.forEach((e) =>{
-        max = Math.max(max , e[1]);
-    });
+    let max = Math.max(...count);
+    let result = [];
+    for(let i = 0 ; i < count.length; i++){
+        if(count[i] === max) result.push(i + 1);
+    }
     
-    return answer.filter((e) => e[1] === max).map((e) => e[0]);
+    return result
 }
