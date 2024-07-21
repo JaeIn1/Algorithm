1a1aac0eb6b8548e6bc9369cc962bf3cc7372f5f
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-07-21 21:16:42 +0900

    [level 1] Title: 실패율, Time: 67.00 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,29 +1,25 @@
 function solution(N, stages) {
-    var answer = [];
-    let arr = [];
+    let answer = [];
+    let result = [];
+    let totalPiple = stages.length;
+    
+    let map = new Map();
+    stages.sort((a , b) => a - b);
+    stages.forEach((e) =>{
+        map.set(e , map.has(e) ? map.get(e) + 1 : 1);
+    });
     
     for(let i = 1; i <= N; i++){
-        let fail = 0;
-        let l = stages.length;
-        let cnt = 0;
-        stages.forEach((e) => {
-            if(e <= i){
-              cnt++;
-            }
-        })
-        fail = cnt / l;
-        arr.push([i , fail]);
-        stages = stages.filter((e) => e > i);
+        if(map.has(i)){
+            let failRate = (map.get(i) / totalPiple);
+            result.push([i , failRate]);
+            totalPiple -= map.get(i);
+        }
+        else{
+            result.push([i , 0]);
+        }
     }
     
-    arr.sort((a , b) =>{
-        if(a[1] === b[1]){
-            return a[0] - b[0];
-        }
-        return b[1] - a[1];
-    });
-    arr.forEach((e) => {
-        answer.push(e[0]);
-    })
-    return answer;
+    result.sort((a , b) => b[1] - a[1]);
+    return result.map((e) => e[0]);
 }
