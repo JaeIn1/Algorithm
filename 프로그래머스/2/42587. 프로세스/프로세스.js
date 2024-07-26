ff9023c45f9d84a5ae89e9f49add74797066a6ac
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-07-26 09:46:31 +0900

    [level 2] Title: 프로세스, Time: 2.40 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,17 @@
+function solution(priorities, location) {
+    const queue = []
+    priorities = priorities.map((priority, index) => [priority, index])
+    
+    while(priorities.length) {
+        const [priority, index] = priorities.shift();
+        const higherImportance = priorities.findIndex(([value, _]) => value > priority)
+        
+        if(higherImportance === -1){
+            if(index === location) return queue.length + 1  
+            queue.push([priority, index])  
+            continue
+        }
+        
+        priorities.push([priority, index]);
+    }
+}
