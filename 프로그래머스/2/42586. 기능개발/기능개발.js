3792ad294ec31a18623d25283740f10b29e8a3d5
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-07-27 16:02:49 +0900

    [level 2] Title: 기능개발, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,30 +1,19 @@
 function solution(progresses, speeds) {
     var answer = [];
-    let stack = [];
+    let queue = [];
+
+    let days = progresses.map((e , i) => Math.ceil((100 - e) / speeds[i]));
+    let max = days[0];
+    let count = 0;
     
     for(let i = 0; i < progresses.length; i++){
-        stack.push(progresses[i]);
-    }
-    
-    while(stack.length > 0){
-        for(let i = 0; i < stack.length; i++){
-            if(stack[i] < 100){
-                stack[i] += speeds[i];
-                if(stack[i] > 100) stack[i] = 100;
-            }
-            else stack[i] = 100;
-        }
-        
-        if(stack[0] === 100){
-            let count = 0;
-            console.log(stack);
-            while(stack[0] === 100){
-                stack.shift();
-                speeds.shift();
-                count++;
-            }
+        if(days[i] <= max) count++;
+        else{
             answer.push(count);
+            count = 1;
+            max = days[i];
         }
     }
+    answer.push(count);
     return answer;
 }
