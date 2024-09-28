1501f951a861b475cd915bb0d6fcbe0d0770451e
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-28 14:51:56 +0900

    [level 2] Title: 타겟 넘버, Time: 376.52 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 412.83 ms
+메모리: 10.1 MB, 시간: 376.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 06일 00:06:29
+2024년 09월 28일 14:51:53
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,17 @@
-def dfs(num, numbers, target):
-    if len(numbers) == 0:
-        if num == target:
+
+
+def dfs(idx, numbers, target , value):
+    if len(numbers) == idx:
+        if value == target: 
             return 1
         return 0
-
-    numOfMethods = 0
-
-    curr_num = numbers[0]
-    next_numbers = numbers[1:]
-    numOfMethods += dfs(num + curr_num, next_numbers, target)
-    numOfMethods += dfs(num - curr_num, next_numbers, target)
-
-    return numOfMethods
-
+    
+    temp = 0
+    temp += dfs(idx + 1 , numbers , target , value + numbers[idx])
+    temp += dfs(idx + 1 , numbers , target , value - numbers[idx])
+    
+    return temp
 
 def solution(numbers, target):
-    answer = dfs(0, numbers, target)
+    answer = dfs(0, numbers, target , 0)
     return answer
