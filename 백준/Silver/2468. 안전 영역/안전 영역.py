f05a5128e3a148f08df04f095c13a6a7bbc3e467
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-29 18:45:00 +0900

    [Silver I] Title: 안전 영역, Time: 692 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,33 +1,44 @@
-import sys
-sys.setrecursionlimit(100000)
+from collections import deque
 
-n = int(sys.stdin.readline().rstrip())
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-
-dx = [-1 , 1 , 0 , 0]
-dy = [0 , 0 , -1 , 1]
-
-def dfs(x, y , h):
-    for i in range(4):
-        nx , ny = x + dx[i] , y + dy[i]
-        if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and graph[nx][ny] > h:
-            visited[nx][ny] = True
-            dfs(nx , ny , h)
-            
-result = 0
-
-for h in range(max(map(max, graph))):
-    area = 0
-    visited = [[False for _ in range(n)] for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if not visited[i][j] and graph[i][j] > h:
-                area += 1
-                visited[i][j] = True
-                dfs(i, j, h)
-    result = max(result, area)
-
-print(result)
-
-        
+n = int(input())
+graph = []
+max_height = 0
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+    max_height = max(max_height, max(graph[-1]))
     
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+    
+def bfs(x, y, r):
+    queue = deque()
+    queue.append((x, y))
+    visited[x][y] = 1
+    
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx<0 or nx>=n or ny<0 or ny>=n:
+                continue
+            if visited[nx][ny] == 1 or graph[nx][ny] <= r:
+                continue
+            queue.append((nx, ny))
+            visited[nx][ny] = 1
+
+result = 0
+for i in range(max_height):
+    count = 0
+    visited = [[0]*n for _ in range(n)]
+    for j in range(n):
+        for k in range(n):
+            if graph[j][k] > i and visited[j][k] == 0:
+                bfs(j, k, i)
+                count += 1
+    result = max(result, count)
+    
+print(result)
+ 
+
+
