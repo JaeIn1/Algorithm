e4e7b4363f43414c6e6c7cc89a5fc989ac63032a
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-29 13:31:37 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+def dfs(start ,path, graph):
+    while graph[start]:
+        next_airport = graph[start].pop(0)
+        dfs(next_airport , path , graph)
+    
+    path.append(start)
+
+
+def solution(tickets):
+    path = []
+    tickets.sort(key = lambda x : (x[0] , x[1]))
+    
+    graph = defaultdict(list)
+    for s , e in tickets:
+        graph[s].append(e)
+        
+    for i in graph:
+        graph[i].sort()
+    
+    start = 'ICN'
+    dfs(start , path , graph)
+    path.reverse()
+    
+    return path
