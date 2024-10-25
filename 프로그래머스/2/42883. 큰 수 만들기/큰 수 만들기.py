201ee9b4f1905be015d9d50f7dd395a5706dcca5
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-10-25 19:16:01 +0900

    [level 2] Title: 큰 수 만들기, Time: 109.52 ms, Memory: 13.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,14 @@
+def solution(number, k):
+    answer = ''
+    stack = []
+    remove = 0
+    
+    for n in number:
+        while stack and stack[-1] < n and remove < k:
+            stack.pop()
+            remove += 1
+        stack.append(n)
+        
+    if remove < k:
+        stack = stack[: -k - remove]
+    return ''.join(stack)
