c6cb333de612fba4cab50a9240efcc8b0b6e4d0c
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-10-29 16:41:45 +0900

    [level 3] Title: N으로 표현, Time: 24.07 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -0,0 +1,24 @@
+def solution(N, number):
+    if N == number:
+        return 1
+    
+    answer = -1
+    arr = [set() for _ in range(8)]
+    
+    for i in range(len(arr)):
+        arr[i].add(int(str(N)*(i+1)))
+    
+    for i in range(1,8):
+        for j in range(i):
+            for op1 in arr[j]:
+                for op2 in arr[i-j-1]:
+                    arr[i].add(op1+op2)
+                    arr[i].add(op1-op2)
+                    arr[i].add(op1*op2)
+                    if op2 != 0:
+                        arr[i].add(op1//op2)
+        if number in arr[i]:
+            answer = i+1
+            break
+    
+    return answer
