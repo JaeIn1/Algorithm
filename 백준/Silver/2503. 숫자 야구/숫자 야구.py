57c6d131250e06dc321f27a8b054bf7bf2ec7794
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-11-03 12:00:05 +0900

    [Silver III] Title: 숫자 야구, Time: 124 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+hint = [list(map(int, input().split())) for _ in range(n)]
+answer = 0
+
+for a in range(1, 10):
+    for b in range(1, 10):
+        for c in range(1, 10):
+            if a == b or b == c or a == c:
+                continue
+            
+            cnt = 0
+            for h in hint:
+                number = str(h[0])
+                strike = h[1] 
+                ball = h[2] 
+            
+                # 볼, 스트라이크 비교
+                ball_cnt = 0
+                strike_cnt = 0
+                
+                checkNum = str(a) + str(b) + str(c)
+                for t in range(3):
+                    if number[t] == checkNum[t]:
+                        strike_cnt += 1
+                    elif number[t] in checkNum:
+                        ball_cnt += 1
+                
+                if strike == strike_cnt and ball == ball_cnt:
+                    cnt += 1
+                    
+            if cnt == n:
+                answer += 1
+
+print(answer)
