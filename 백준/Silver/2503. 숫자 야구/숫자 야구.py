1c84e23d135ae4a4e0870c7a2f6acc5039f00ff4
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-11-03 12:01:07 +0900

    [Silver III] Title: 숫자 야구, Time: 120 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -2,10 +2,10 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-hint = [list(map(int, input().split())) for _ in range(n)]
+hint = [list(map(int , input().split())) for _ in range(n)]
 answer = 0
 
-for a in range(1, 10):
+for a in range(1 , 10):
     for b in range(1, 10):
         for c in range(1, 10):
             if a == b or b == c or a == c:
@@ -14,10 +14,10 @@ for a in range(1, 10):
             cnt = 0
             for h in hint:
                 number = str(h[0])
-                strike = h[1] 
-                ball = h[2] 
+                strike = h[1]
+                ball = h[2]
             
-                # 볼, 스트라이크 비교
+                # 볼 , 스트라이크 비교
                 ball_cnt = 0
                 strike_cnt = 0
                 
@@ -28,10 +28,10 @@ for a in range(1, 10):
                     elif number[t] in checkNum:
                         ball_cnt += 1
                 
-                if strike == strike_cnt and ball == ball_cnt:
+                if ball == ball_cnt and strike == strike_cnt:
                     cnt += 1
                     
             if cnt == n:
                 answer += 1
-
+            
 print(answer)
