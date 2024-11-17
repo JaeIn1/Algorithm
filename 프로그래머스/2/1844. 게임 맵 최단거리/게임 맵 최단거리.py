be6823881a286d7664f47aaabc0cb4d61f66f99d
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-11-17 11:57:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.62 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,28 +1,32 @@
 from collections import deque
-
-def checkRange(x , y , x_h , y_h):
-    return x >= x_h or x < 0 or y >= y_h or y < 0
-
-
-def bfs(maps ,visited , x_h , y_h):
-    dx = [1 , -1 , 0 , 0]
-    dy = [0 , 0 , -1 , 1]
-
-    queue = deque([(0, 0, 1)])
     
-    while len(queue) > 0:
-        x , y , value = queue.popleft()
-        
-        for i in range(4):
-            nx , ny = x + dx[i] , y + dy[i]
-            if not checkRange(nx, ny, x_h, y_h) and not visited[nx][ny] and maps[nx][ny] == 1:
-                queue.append([nx , ny , value + 1])
-                visited[nx][ny] = True
-                if nx == x_h - 1 and ny == y_h - 1: return value + 1
-            
-                             
+    
 def solution(maps):
-    x_h , y_h = len(maps) , len(maps[0])
-    visited = [[False] * y_h for _ in range(x_h)]
-    result = bfs(maps , visited , x_h , y_h)
-    return result if result else -1
+    answer = 0
+    l_x = len(maps) # 행
+    l_y = len(maps[0]) # 열
+     
+    visite = [[0] * l_y for _ in range(l_x)]
+    dx = [-1 , 1 , 0 , 0]
+    dy = [0 , 0 , -1 , 1]
+    
+      
+    def bfs(x , y):
+        queue = deque([(x , y , 1)])
+        
+        while queue:
+            cx , cy , ct = queue.popleft()
+            if cx == l_x - 1 and cy == l_y - 1:
+                return ct
+            
+            for i in range(4):
+                nx = cx + dx[i]
+                ny = cy + dy[i]
+                
+                if 0 <= nx < l_x and 0 <= ny < l_y:
+                    if maps[nx][ny] == 1 and visite[nx][ny] == 0:
+                        queue.append((nx , ny , ct + 1))
+                        visite[nx][ny] = 1
+        return -1
+        
+    return bfs(0 , 0)
