f5b4edfa4c36fa24433cd3c6d72fe3ba10b7a0cd
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-11-17 11:16:13 +0900

    [level 2] Title: 더 맵게, Time: 1845.66 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,18 +1,21 @@
-from heapq import heappop , heappush , heapify
+from heapq import heappop , heappush
 
 def solution(scoville, K):
-    heapify(scoville)
+    h = []
+    for i in scoville:
+        heappush(h , i)
+        
     answer = 0
 
     while True:
         
-        if scoville[0] >= K: break
-        if len(scoville) < 2:
+        if h[0] >= K: break # 아직 정렬된 상태는 아니지만 항상 맨 앞은 가장 작은 수로 정해진다.
+        if len(h) < 2:
             return -1
         
-        first = heappop(scoville)
-        second = heappop(scoville)
-        heappush(scoville, first + (second * 2))
+        first = heappop(h)
+        second = heappop(h)
+        heappush(h, first + (second * 2))
         
         answer += 1
     
