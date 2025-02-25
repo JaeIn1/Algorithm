a9514af855cfe8b55e49b07a773f5d9745992ecb
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-02-26 07:57:40 +0900

    [level 2] Title: 올바른 괄호, Time: 9.42 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,11 +1,19 @@
 def solution(s):
-    answer = True
     stack = []
-    
-    for i in s:
-        if i == '(': stack.append('(')
-        elif i == ')' and len(stack) > 0: stack.pop()
-        else: return False
 
-    if len(stack) > 0: answer = False
-    return answer
+    
+    for str in s:
+        if str == "(":
+            stack.append(str)
+        else:
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    
+    if len(stack):
+        return False
+    else:
+        return True
+    
+    
