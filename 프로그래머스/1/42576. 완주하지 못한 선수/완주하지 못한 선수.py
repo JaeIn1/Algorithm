a9fa7dda61862ea7cb9cebfc4e8b06a8c546ce60
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-02-26 07:54:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 44.40 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,17 @@
 from collections import Counter
 
 def solution(participant, completion):
-    
     d = dict(Counter(participant))
     
     for c in completion:
         if c in d:
             d[c] -= 1
             
-
+    
     for key , value in d.items():
         if value > 0:
             return key
+        
+    
     
     
