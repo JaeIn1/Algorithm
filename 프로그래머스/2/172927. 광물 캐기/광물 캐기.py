ed459a0e3228032c3228fb27e7932080c4b1bb27
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-02-28 21:22:15 +0900

    [level 2] Title: 광물 캐기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -4,14 +4,10 @@ def solution(picks, minerals):
     for x in picks:
         sum += x
     
-    # 해당 길이까지 자르기 위한 스킬
-    # 총 개수를 먼저 구한 뒤 그만큼까지 자른다.
     num_min = sum * 5
     if len(minerals) > num_min:
         minerals = minerals[:num_min]
     
-    # 각각 자른만큼 개수를 세기위해 // 연산자를 사용해서
-    # 만약 5개씩 자른다고 하면 //5를 이용해서 카운트한다!
     cnt_min = [[0, 0, 0]for x in range(10)] 
     for i in range(len(minerals)):
         if minerals[i] == 'diamond': 
@@ -21,11 +17,9 @@ def solution(picks, minerals):
         else : 
             cnt_min[i//5][2] += 1
     
-    
-    # 다이아, 철, 돌 순으로 내림차순 정렬을 위한 코드
+
     sorted_cnt_min = sorted(cnt_min, key = lambda x: (-x[0], -x[1], -x[2]))
     
-    # 있거나 1개 이상인 경우를 보기 위해 체크
     for mineral in sorted_cnt_min:
         d, i, s = mineral
         for p in range(len(picks)):
