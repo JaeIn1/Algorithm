0b60b75f0001d34f6bc2d7044b1e18aec2c59e66
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-20 21:13:16 +0900

    [Gold II] Title: 로봇 조종하기, Time: 1368 ms, Memory: 94980 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
@@ -1,40 +1,29 @@
-import copy
-N , M = map(int , input().split())
+n, m = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * m for _ in range(n)]
 
-weight = []
-for _ in range(N):
-    weight.append(list(map(int , input().split())))
-    
+# 첫 행은 수동으로 복사하고 누적합 구하기
+for i in range(m):
+    dp[0][i] = data[0][i]
+    if i > 0:
+        dp[0][i] += dp[0][i-1]
 
-dp = [[0] * M for _ in range(N)]
-dp[0] = copy.deepcopy(weight[0])
-
-
-for i in range(1 , M):
-    dp[0][i] += dp[0][i - 1]
+for i in range(1, n):
+    tmp1 = [0] * m  # 왼쪽 > 오른쪽 최대값
+    tmp2 = [0] * m  # 오른쪽 > 왼쪽 최대값
     
-    
-for i in range(1 , N):
-    temp1 = [0] * M
-    temp2 = [0] * M
-    
-    for j in range(0 , M):
+    for j in range(m):
+        # 첫번째 칸은 그냥 갱신
         if j == 0:
-            temp1[j] = weight[i][j] + dp[i - 1][j]
-            temp2[M - 1 - j] = weight[i][M - 1 - j] + dp[i - 1][M - 1 - j]
+            tmp1[j] = data[i][j] + dp[i-1][j]
+            tmp2[m-1-j] = data[i][m-1-j] + dp[i-1][m-1-j]
             continue
-            
-        # 왼쪽
-        temp1[j] = weight[i][j] + max(dp[i - 1][j] , temp1[j - 1])
         
-        # 오른쪽
-        temp2[M - 1- j] = weight[i][M - 1 - j] + max(dp[i - 1][M - 1 - j] , temp2[M - j])
+        tmp1[j] = data[i][j] + max(dp[i-1][j], tmp1[j-1])
+        tmp2[m-1-j] = data[i][m-1-j] + max(dp[i-1][m-1-j], tmp2[m-j])
     
-    temp = [max(temp1[i] , temp2[i]) for i in range(M)]
-    dp[i] = copy.deepcopy(temp)
-    
-    
-print(dp[N - 1][M - 1])
-        
-        
-        
+    # tmp1과 tmp2 중 최대값을 dp에 갱신 (수동으로 복사)
+    for j in range(m):
+        dp[i][j] = max(tmp1[j], tmp2[j])
+
+print(dp[n-1][m-1])
