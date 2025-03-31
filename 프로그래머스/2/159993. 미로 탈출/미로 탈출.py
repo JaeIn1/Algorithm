9f72f02d77530a05de67770ba55aa3f15124bb60
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-03-31 18:40:47 +0900

    [level 2] Title: 미로 탈출, Time: 15.37 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,46 +1,50 @@
 from collections import deque
-    
-def bfs(start, end, maps):
-    answer = 0
-    
-    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-    row, col = len(maps), len(maps[0])
-                              
-    check = [[0] * col for _ in range(row)]
-    
-    for i in range(len(maps)):
-        for j in range(len(maps[0])):
-            if maps[i][j] == start:
-                start = [i, j]
-            elif maps[i][j] == end:
-                end = [i, j]
-    
-    x, y = start[0], start[1]
 
-    q = deque()
-    q.append([x, y, 0])    
+def bfs(start , end , maps):
+    dx = [-1 , 1 , 0 , 0]
+    dy = [0 , 0 , -1 , 1]
     
-    while(q):
-        x, y, res = q.popleft()
+    row , col = len(maps) , len(maps[0])
+
+    for i in range(row):
+        for j in range(col):
+            if maps[i][j] == start:
+                start = [i , j]
+            elif maps[i][j] == end:
+                end = [i , j]
+                
+    
+    q = deque()
+    q.append([start[0] , start[1] , 0])
+    visited = [[0] * col for _ in range(row)]
+
+    
+    while q:
+        x , y , dis = q.popleft()
         
         if x == end[0] and y == end[1]:
-            return res
-
+            return dis
+        
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
             
             if 0 <= nx < row and 0 <= ny < col:
-                if check[nx][ny] == 0 and maps[nx][ny] != "X":
-                    check[nx][ny] = 1
-                    q.append([nx, ny, res+1])
-            
+                if visited[nx][ny] == 0 and maps[nx][ny] != 'X':
+                    q.append([nx , ny , dis + 1])
+                    visited[nx][ny] = 1
+
     return -1
-                              
+
 def solution(maps):
-    to_lever = bfs("S", "L", maps)  # '시작 지점 -> 레버' 경로의 거리 구하기
-    to_end = bfs("L", "E", maps)    # '레버 -> 출구' 경로의 거리 구하기
-         
-    if to_lever != -1 and to_end != -1:
-        return to_lever + to_end
+    answer = 0
+       
+    laber = bfs('S' , 'L' , maps)
+    if laber == -1:
+        return -1
     
-    return -1
+    exit = bfs('L' , 'E' , maps)
+    if exit == -1:
+        return -1
+        
+    return laber + exit
