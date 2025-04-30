45a2784f49d82d7ad672946f9ace2804036337dc
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-04-30 12:15:25 +0900

    [level 2] Title: 문자열 압축, Time: 2.75 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.98 MB, 시간: 4.13 ms
+메모리: 9.31 MB, 시간: 2.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 20일 18:34:48
+2025년 04월 30일 12:15:22
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,29 +1,36 @@
 def solution(s):
-    answer = []
-    l = len(s)
-    if l == 1:
-        return 1
+    n = len(s)
+    answer = n
     
-    for i in range(1 ,l//2 + 1):
-        st = ""
-        temp = s[:i]
-        cnt = 1
+    
+    for i in range(1 , n // 2 + 1):
+        splited = []
         
-        for j in range(i , l , i):
-            if temp == s[j:j + i]:
-                cnt += 1
+        # 나눠서 배열에 저장
+        for j in range(0, n, i):
+            splited.append(s[j : j + i])
+            
+        count = 1
+        # 압축된 문자열 저장 변수
+        temp_str = ""
+        for i in range(0 , len(splited) - 1):
+            cur , next = splited[i] , splited[i + 1]
+            if cur == next:
+                count += 1
             else:
-                if cnt > 1:
-                    st += str(cnt) + temp
+                if count == 1:
+                    temp_str += cur                    
                 else:
-                    st += temp
-                cnt = 1
-                temp = s[j:j + i]
-                
-        if cnt > 1:
-            st += str(cnt) + temp
-        else:
-            st += temp
-        answer.append(len(st))
+                    temp_str += f"{count}{cur}"
+                count = 1
         
-    return min(answer)
+        # 마지막에 원소가 남았을 때
+        if count == 1:
+            temp_str += splited[-1]
+        else:
+            temp_str += f"{count}{splited[-1]}"
+        
+        answer = min(answer , len(temp_str))
+        
+        
+    return answer
