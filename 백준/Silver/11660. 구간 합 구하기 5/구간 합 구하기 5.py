45874808b0a82bf47c4b9d637ebb5791a62cec7e
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-05-05 15:39:11 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 3568 ms, Memory: 132668 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,16 +1,16 @@
-n , k = map(int , input().split())
+N , M = map(int, input().split())
+graph = [list(map(int , input().split())) for _ in range(N)]
+search = [list(map(int , input().split())) for _ in range(M)]
 
-board = [list(map(int , input().split())) for _ in range(n)]
-arr = [list(map(int , input().split())) for _ in range(k)]
+prefix = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
 
-prefix = [[0] * (n + 1) for _ in range(n + 1)]
+for x in range(N):
+    for y in range(N):
+        prefix[x + 1][y + 1] = prefix[x][y + 1] + prefix[x + 1][y] - prefix[x][y] + graph[x][y]
 
-for i in range(n):
-    for j in range(n):
-        prefix[i + 1][j + 1] = prefix[i][j + 1] + prefix[i + 1][j] + board[i][j] - prefix[i][j]
-        
-for x1 , y1 , x2 , y2 in arr:
-    print(prefix[x2][y2] - prefix[x2][y1 - 1] - prefix[x1 - 1][y2] + prefix[x1 - 1][y1 - 1])
-    
-            
-        
+
+for s in search:
+    x1 , y1 , x2 , y2 = s
+    print(prefix[x2][y2] - prefix[x1-1][y2] - prefix[x2][y1-1] + prefix[x1-1][y1-1])
+
+
