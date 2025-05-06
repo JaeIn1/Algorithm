62d2107a2adaac9a7779f69b34e3c57364415ce9
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-05-06 13:50:45 +0900

    [Silver II] Title: 창고 다각형, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,37 +1,46 @@
 N = int(input())
-graph = [0] * 1001
+graph = [0] * 1001 # 기둥의 개수만큼 초기화
 
 x_list = []
 y_list = []
+
 for i in range(N):
-    x, y = map(int, input().split())
-    graph[x] = y
+    x ,y = map(int , input().split())
     x_list.append(x)
     y_list.append(y)
+    graph[x] = y
+    
 
 max_x = max(x_list)
 max_y = max(y_list)
 prefix = [0] * (max_x + 2)
 suffix = [0] * (max_x + 2)
 
+
 max_point = []
+# 앞에서부터 누적합
 h = 0
-
-for f in range(1, max_x + 3):
-    if graph[f] == max_y:
-        max_point.append(f)
+for i in range(1, max_x + 3):
+    if graph[i] == max_y:
+        max_point.append(i)
         break
-    h = max(graph[f], h)
-    prefix[f] = prefix[f - 1] + h
+    h = max(graph[i] , h)
+    prefix[i + 1] = prefix[i] + h
+    
 
+ # 뒤에서부터 누적합
 h = 0
-for f in range(max_x, 0, -1):
-    if graph[f] == max_y:
-        max_point.append(f)
+for i in range(max_x , 0 , -1):
+    if graph[i] == max_y:
+        max_point.append(i)
         break
-    h = max(graph[f], h)
-    suffix[f] = suffix[f + 1] + h
-
+    h = max(graph[i] , h)
+    suffix[i] = suffix[i + 1] + h
+    
 answer = max(prefix) + max(suffix)
 answer += (max_point[1] - max_point[0] + 1) * max_y
+
+    
 print(answer)
+
+
