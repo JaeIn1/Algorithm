13606e332b4844b411b30ffad3d062403be96498
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-05-07 14:33:48 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.js
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.js
@@ -1,31 +1,26 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 let n = Number(input[0]);
-let arr = [];
-for(let i = 1; i <= n; i++) arr.push(input[i].split(" ").map(Number));
-let visited = new Array(n).fill(false);
-let result = [];
-let min = 1e9;
-
-function dfs(depth , start){
-  if(depth >= 1){
-    let totalX = 1;
-    let totalY = 0;
-    for(let i of result){
-      let [a,b] = arr[i];
-      totalX *= a;
-      totalY += b;
-    }
-    min = Math.min(min , Math.abs(totalX - totalY));
-  }
-  for(let i = start; i < n; i++){
-    if(visited[i]) continue;
-    result.push(i);
-    visited[i] = true;
-    dfs(depth + 1 , i + 1);
-    visited[i] = false;
-    result.pop();
-  }
+let ingre = [];
+for (let i = 1; i <= n; i++) {
+  ingre.push(input[i].split(" ").map(Number));
 }
-dfs(0 , 0);
-console.log(min);
+
+let answer = 1e9;
+
+function dfs(idx, sin, jan, use) {
+  if (idx == n) {
+    if (use == 0) return;
+
+    let diff = Math.abs(sin - jan);
+    answer = Math.min(answer, diff);
+    return;
+  }
+
+  dfs(idx + 1, sin * ingre[idx][0], jan + ingre[idx][1], use + 1);
+  dfs(idx + 1, sin, jan, use);
+}
+
+dfs(0, 1, 0, 0);
+
+console.log(answer);
