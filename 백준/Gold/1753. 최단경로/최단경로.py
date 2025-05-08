bd66d88b22673b94f32c121d7b4551a919adec16
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-05-08 21:52:25 +0900

    [Gold IV] Title: 최단경로, Time: 856 ms, Memory: 74420 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -3,32 +3,31 @@ import heapq
 
 input = sys.stdin.readline
 
-N , M = list(map(int , input().split()))
+V , E = map(int , input().split())
 start = int(input())
+dist = [1e9 for _ in range(V + 1)]
 
-arr = [[] for _ in range(N + 1)]
-dist = [1e9 for _ in range(N + 1)]
+graph = [[] for _ in range(V + 1)]
 
-for l in range(M):
-    a , b, c = list(map(int , input().split()))
-    arr[a].append([b , c])
+for _ in range(E):
+    a , b , c = map(int , input().split())
+    graph[a].append([b , c])
     
 
 q = []
-heapq.heappush(q , [0,start])
 dist[start] = 0
-
-
+heapq.heappush(q , [0 , start])
 
 while q:
     _w , node = heapq.heappop(q)
-
-    for nxt , weight in arr[node]:
+    
+    for nxt , weight in graph[node]:
         if dist[node] + weight < dist[nxt]:
             dist[nxt] = dist[node] + weight
-            heapq.heappush(q,  [dist[nxt] , nxt])
-    
-
+            heapq.heappush(q , [dist[nxt],  nxt])
+            
+        
+        
 for i in range(1 , len(dist)):
     if dist[i] == 1e9:
         print('INF')
