a16c1385a7382db994364cade374f78790f9df60
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-05-09 11:47:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 17.28 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 12.61 ms
+메모리: 38.4 MB, 시간: 17.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 26일 10:30:22
+2025년 05월 09일 11:47:37
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,66 +1,38 @@
 function solution(maps) {
-    var answer = 0;
-    
-    class Queue{
-        constructor() {
-            this.q = [];
-            this.head = -1;
-            this.tail = -1;
-        }
-        empty() {
-            return this.head === this.tail;
-        }
-        size() {
-            return this.tail - this.head;
-        }
-        push(item){
-            this.q.push(item);
-            this.tail++;
-        }
-        pop() {
-        if (this.empty()) {
-            throw new Error("Queue is empty");
-        }
-        return this.q[++this.head];
-    }
-    }
-    
     let n = maps.length;
     let m = maps[0].length;
-    let visited = Array.from(Array(n) , () => new Array(m).fill(false));
-    let queue = new Queue();
-    let flag = false;
-    let dx = [1 ,-1 , 0 , 0];
-    let dy = [0 , 0 , 1,  -1];
-    function inRange(x , y){
-        return 0 <= x && x < n && 0 <= y && y < m;
-    }
-    
-    function canGo(x , y){
-        return inRange(x , y) && maps[x][y] === 1 && !visited[x][y];
-    }
-    
-    function bfs(){
+    let visited = Array.from(Array(n), () => new Array(m).fill(false));
+    let q = [];
+
+    let dx = [1, -1, 0, 0];
+    let dy = [0, 0, 1, -1];
+
+    function bfs() {
         visited[0][0] = true;
-        queue.push([0 , 0 , 1]);
-        
-        while(queue.size() > 0){
-            let [x , y,  dis] = queue.pop();
-            if(x === n - 1 && y === m - 1){
-                flag = true;
+        q.push([0, 0, 1]);
+
+        while (q.length > 0) {
+            let [cx, cy, dis] = q.shift(); // BFS는 shift
+
+            if (cx === n - 1 && cy === m - 1) {
                 return dis;
             }
-            for(let i = 0; i < 4; i++){
-                let nx = x + dx[i];
-                let ny = y + dy[i];
-                if(canGo(nx , ny)){
-                    queue.push([nx , ny , dis + 1]);
-                    visited[nx][ny]= true;
+
+            for (let i = 0; i < 4; i++) {
+                let nx = cx + dx[i];
+                let ny = cy + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    if (!visited[nx][ny] && maps[nx][ny] === 1) {
+                        visited[nx][ny] = true;
+                        q.push([nx, ny, dis + 1]);
+                    }
                 }
             }
         }
+
         return -1;
     }
-    
+
     return bfs();
 }
