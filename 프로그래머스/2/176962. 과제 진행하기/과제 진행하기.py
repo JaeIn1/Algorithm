9e92e025ccc97754d3905e67228d352110f3c13d
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-06-17 12:49:57 +0900

    [level 2] Title: 과제 진행하기, Time: 2.70 ms, Memory: 9.79 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,39 +1,39 @@
-def change_time(time):
-    hours, minutes = map(int,time.split(':'))
-    return int(minutes + (hours*60))
-
 def solution(plans):
     answer = []
-    left_table = []
-    left_time = 0
-    for plan in plans:
-        plan[1] = change_time(plan[1])
-    plans.sort(key=lambda x: x[1])
-
-    for i, (plan_1, plan_2) in enumerate(zip(plans, plans[1:])):
-        pred_end, tail_start = plan_1[1]+int(plan_1[2]), plan_2[1]
-
-        if pred_end > tail_start:
-            left_table.append([plan_1[0], pred_end - tail_start])
+    for i in range(len(plans)):
+        h, m = map(int, plans[i][1].split(':'))
+        st = h*60+m
+        plans[i][1] = st
+        plans[i][2] = int(plans[i][2])
+        
+    plans.sort(key=lambda x:x[1])
+    stack = []
+    for i in range(len(plans)):
+        if i == len(plans)-1:
+            stack.append(plans[i])
+            break
+        
+        sub, st, t = plans[i]
+        nsub, nst, nt = plans[i+1]
+        if st + t <= nst:
+            answer.append(sub)
+            temp_time = nst - (st+t)
+            
+            while temp_time != 0 and stack:
+                tsub, tst, tt = stack.pop()
+                if temp_time >= tt:
+                    answer.append(tsub)
+                    temp_time -= tt
+                else:
+                    stack.append([tsub, tst, tt - temp_time])
+                    temp_time = 0
+            
         else:
-            answer.append(plan_1[0])
-            left_time = tail_start - pred_end
-
-            while left_time > 0:
-                if left_table:
-                    plan = left_table.pop()
-                    left_time = left_time - plan[1]
-                else:
-                    break
-
-                if left_time >= 0 :
-                    answer.append(plan[0])
-                else:
-                    plan[1] = -left_time
-                    left_table.append(plan)
-
-    answer.append(plans[-1][0])
-    while left_table:
-        answer.append(left_table.pop()[0])
+            plans[i][2] = t - (nst - st)
+            stack.append(plans[i])
+        
+    while stack:
+        sub, st, tt = stack.pop()
+        answer.append(sub)
 
     return answer
