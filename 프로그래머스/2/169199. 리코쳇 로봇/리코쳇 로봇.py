b17289e092a36cf1dcf2936e5e660a83357dac98
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-06-20 18:32:39 +0900

    [level 2] Title: 리코쳇 로봇, Time: 10.65 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,49 +1,53 @@
 from collections import deque
 
 def solution(board):
-    answer = 0
-    R = len(board)
-    C = len(board[0])
-    rx, ry = 0, 0
-    for i in range(R):
-        for j in range(C):
-            if board[i][j] == 'R':
-                rx, ry = i, j
-                
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
     
+    x_l = len(board)
+    y_l = len(board[0])
+    
+    s_x, s_y = 0 , 0
+    
+    dx = [1 , -1 , 0 , 0]
+    dy = [0 , 0 , -1 , 1]
+    
+    for i in range(x_l):
+        for j in range(y_l):
+            if board[i][j] == 'R':
+                s_x, s_y = i , j
+                
     def bfs():
         q = deque()
-        q.append((rx, ry))
-        visited = [[0]*C for _ in range(R)]
-        visited[rx][ry] = 1
-        
-        while q:
-            px, py = q.popleft()
-            if board[px][py] == 'G':
-                return visited[px][py]
-            
-            # 한칸씩 가는게 아닌 D를 만날때까지 간다고 하면 while문을 이용
-            for i in range(4):
-                nx, ny = px, py
-                while True:
-                    nx, ny = nx + dx[i], ny + dy[i]
-                    if 0 <= nx < R and 0 <= ny < C and board[nx][ny] == 'D':
-                        nx -= dx[i]
-                        ny -= dy[i]
-                        break
-                    if nx < 0 or nx >= R or ny < 0 or ny >= C:
-                        nx -= dx[i]
-                        ny -= dy[i]
-                        break
+        q.append((s_x , s_y))
+        visited = [[0] * y_l for _ in range(x_l)]
+        visited[s_x][s_y] = 1
                 
-                if 0 <= nx < R and 0 <= ny < C and visited[nx][ny] == 0:
-                    visited[nx][ny] = visited[px][py] + 1
-                    q.append((nx, ny))
-        
-        return -1
+        while q:
+            x , y = q.popleft()
+            if board[x][y] == 'G':
+                return visited[x][y]
+            
+            for i in range(4):
+                nx , ny = x , y
+                while True:
+                    nx = dx[i] + nx
+                    ny = dy[i] + ny
+            
+                    if 0 <= nx < x_l and 0 <= ny < y_l and board[nx][ny] == 'D':
+                        nx -= dx[i]
+                        ny -= dy[i]
+                        break
+                    elif 0 > nx or nx >= x_l or 0 > ny or ny >= y_l:
+                        nx -= dx[i]
+                        ny -= dy[i]
+                        break
+                        
+                if 0 <= nx < x_l and 0 <= ny < y_l and visited[nx][ny] == 0:
+                    visited[nx][ny] = visited[x][y] + 1
+                    q.append((nx , ny))
                     
+        return -1
+                        
+                        
     answer = bfs()
     if answer > 0:
         answer -= 1
