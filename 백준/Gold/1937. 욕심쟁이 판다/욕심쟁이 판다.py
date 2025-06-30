76ef8e463637d784b4a2af3275b8cf1d3e771bc8
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-06-30 23:56:15 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 728 ms, Memory: 104860 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -1,34 +1,33 @@
 import sys
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(99999999)
 
 N = int(input())
+
 forest = [list(map(int, input().split())) for _ in range(N)]
 
-dp = [[0] * N for _ in range(N)]
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+
+dp = [[0 for _ in range(N)] for _ in range(N)]
 
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def dfs(x, y):
+def dfs(x , y):
+    
     if dp[x][y] != 0:
         return dp[x][y]
-
+    
     dp[x][y] = 1
     
     for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-
+        nx , ny = x + dx[i] , y + dy[i]
         if 0 <= nx < N and 0 <= ny < N:
-            if forest[nx][ny] > forest[x][y]:
-                dp[x][y] = max(dp[x][y], dfs(nx, ny) + 1)
-
+            if forest[x][y] < forest[nx][ny]:
+                dp[x][y] = max(dfs(nx, ny) + 1 , dp[x][y])
     return dp[x][y]
-
 
 answer = 0
 for i in range(N):
     for j in range(N):
-        answer = max(answer, dfs(i, j))
-
+        answer = max(dfs(i, j) ,answer)
+        
 print(answer)
