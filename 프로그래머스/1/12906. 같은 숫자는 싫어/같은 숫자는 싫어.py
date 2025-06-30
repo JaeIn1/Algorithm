d3026dc4bbc632672c0dd2a5ad0fe22d00343e1f
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-07-01 00:36:06 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 84.36 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,9 +1,13 @@
 def solution(arr):
-    stack = [arr[0]]
+    answer = []
     
-    for i in range(1 , len(arr)):
+    stack = []
+    stack.append(arr[0])
+    
+    for i in range(1, len(arr)):
         if stack[-1] == arr[i]:
             continue
-        else: stack.append(arr[i])
+        else:
+            stack.append(arr[i])
     
     return stack
